dcda1ba486ce3c8be36bbcc7065fa71533821071
mladendjordjevic <mladendjordjevic92nis@gmail.com>
2018-10-24 16:04:51 +0200

    NOVA

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../interfaces'
+
 
 @Component({
   selector: 'app-current-weather',
-  template: '<p> current-weather works! </p>',
-  styles: ['']
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
-
-  constructor() { }
+  current: ICurrentWeather
+  constructor() {
+    this.current = {
+      city: 'Nis',
+      country: 'Srbija',
+      date: new Date(),
+      Image: 'http://cdn1.shopmania.biz/files/s4/773291108/p/l/8/slike-na-platnu-vuk-apstraktno-nina085-k~4618.jpg',
+      temperature: 72,
+      description: 'suny'
+    }as ICurrentWeather
+   }
 
   ngOnInit() {
   }
