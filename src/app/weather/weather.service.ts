7ee108b7bd142ed6c326d0a7950ef2a2465b389a
mladendjordjevic <mladendjordjevic92nis@gmail.com>
2018-10-24 20:40:23 +0200

    qqq

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
+interface ICurrentWetherData {
+  weather: [{
+    description: string,
+    icon: string
+  }],
+  main: {
+    temp: number
+  },
+  sys: {
+    country: string
+  },
+  dt: number,
+  name: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
